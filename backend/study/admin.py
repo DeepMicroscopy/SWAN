75dbae7ef0956180663cdf2ffb69e23bc1907ab0
klassiker <6598117+klassiker@users.noreply.github.com>
2025-07-28 00:00:03 +0200

    Make studies look better for admins and add some views

--- backend/study/admin.py
+++ backend/study/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from study.models import Dataset, Study, Classification
 
-admin.site.register(Dataset)
-admin.site.register(Study)
-admin.site.register(Classification)
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ['title', 'archive', 'file_count']
+    exclude = ['file_count']
+    readonly_fields = ['file_list']
+
+
+@admin.register(Study)
+class StudyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'pub_date', 'end_date', 'group', 'dataset', 'ui']
+
+
+@admin.register(Classification)
+class ClassificationAdmin(admin.ModelAdmin):
+    pass
--- backend/study/models.py
+++ backend/study/models.py
@@ -1,24 +1,60 @@
 import datetime
 import uuid
 
+import tarfile
+import zipfile
+
 from django.contrib.auth.models import Group, User
 from django.db import models
 
 from models.base import UUIDModel
 
-def upload_to_dataset(instance, filename):
+def upload_to(name):
     today = datetime.date.today()
-    return f'datasets/{today.year}/{today.month}/{today.day}/{uuid.uuid4().hex}'
+    return f'{name}/{today.year}/{today.month}/{today.day}/{uuid.uuid4().hex}'
+
+def upload_to_dataset(instance, filename):
+    return upload_to("dataset")
+
+def upload_to_image(instance, filename):
+    return upload_to("image")
+
+def extract_file_names(file_path):
+    if zipfile.is_zipfile(file_path):
+        with zipfile.ZipFile(file_path, 'r') as zipf:
+            return zipf.namelist()
+    elif tarfile.is_tarfile(file_path):
+        with tarfile.open(file_path, 'r:*') as tarf:
+            return tarf.getnames()
+    else:
+        return []
 
 class Dataset(UUIDModel):
     title = models.CharField(max_length=200)
     archive = models.FileField(upload_to=upload_to_dataset)
+    file_count = models.PositiveIntegerField(null=True, blank=True)
+    file_list = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.archive:
+            self.file_list = extract_file_names(self.archive.path)
+            self.file_count = len(self.file_list)
+
+            super().save(update_fields=['file_list', 'file_count'])
 
 class UiType(models.IntegerChoices):
     DEFAULT = 1, 'Default'
 
 class Study(UUIDModel):
+    objects = models.Manager()
+
     title = models.CharField(max_length=200)
+    image = models.FileField(upload_to=upload_to_image)
 
     pub_date = models.DateTimeField("publication date")
     end_date = models.DateTimeField("date the study ends")
@@ -30,6 +66,9 @@ class Study(UUIDModel):
         choices=UiType.choices,
         default=UiType.DEFAULT
     )
+
+    def __str__(self):
+        return self.title
 
 class Classification(UUIDModel):
     date = models.DateTimeField("entry time")
