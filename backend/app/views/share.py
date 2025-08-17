9c061640433ee22d592768cb0643a92c7146524b
klassiker <6598117+klassiker@users.noreply.github.com>
2025-08-17 20:04:30 +0200

    Fix qr code after moving things

--- backend/app/views/share.py
+++ backend/app/views/share.py
@@ -1,12 +1,11 @@
+from app import util
 from django.http.response import HttpResponse
 from drf_spectacular.utils import extend_schema, OpenApiTypes, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
-from app import util
 
-
-@extend_schema(tags=['Share'], parameters=[OpenApiParameter("study", OpenApiTypes.UUID, OpenApiParameter.PATH)])
+@extend_schema(tags=['Share'])
 class ShareViewSet(viewsets.ViewSet):
     lookup_url_kwarg = "study"
 
@@ -15,10 +14,11 @@ class ShareViewSet(viewsets.ViewSet):
     @extend_schema(
         responses=OpenApiTypes.STR,
         description="A QR code linking to this study",
+        parameters=[OpenApiParameter("study", OpenApiTypes.UUID, OpenApiParameter.PATH)],
     )
     @action(detail=True, renderer_classes=[util.SVGRenderer])
-    def users(self, request, pk=None):
+    def users(self, request, study=None):
         return HttpResponse(
-            util.create_qr(request.build_absolute_uri(f"/#/studies/{pk}")),
+            util.create_qr(request.build_absolute_uri(f"/#/studies/{study}")),
             content_type="image/svg+xml"
         )
