21725865b9e63080f7bb1746f0ee110a85697c03
Sara Hester <11396751+S4ra@users.noreply.github.com>
2025-08-20 22:43:12 +0200

    Delete vuetify icons from footer

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -3,21 +3,6 @@
     app
     height="40"
   >
-    <a
-      v-for="item in items"
-      :key="item.title"
-      class="d-inline-block mx-2 social-link"
-      :href="item.href"
-      rel="noopener noreferrer"
-      target="_blank"
-      :title="item.title"
-    >
-      <v-icon
-        :icon="item.icon"
-        :size="item.icon === '$vuetify' ? 24 : 16"
-      />
-    </a>
-
     <div
       class="text-caption text-disabled"
       style="position: absolute; right: 16px;"
@@ -34,26 +19,6 @@
     </div>
   </v-footer>
 </template>
-
-<script setup lang="ts">
-  const items = [
-    {
-      title: 'Vuetify Documentation',
-      icon: `$vuetify`,
-      href: 'https://vuetifyjs.com/',
-    },
-    {
-      title: 'Vuetify Support',
-      icon: 'mdi-shield-star-outline',
-      href: 'https://support.vuetifyjs.com/',
-    },
-    {
-      title: 'Vuetify GitHub',
-      icon: `mdi-github`,
-      href: 'https://github.com/vuetifyjs/vuetify',
-    },
-  ]
-</script>
 
 <style scoped lang="sass">
   .social-link :deep(.v-icon)
