4b9da9292b08240ce86e3f3b567455311fd38e4c
klassiker <6598117+klassiker@users.noreply.github.com>
2025-08-20 23:45:50 +0200

    Use ImageField for study title image

--- backend/app/models.py
+++ backend/app/models.py
@@ -71,7 +71,7 @@ class Ui(UUIDModel):
 
 class Study(UUIDModel):
     title = models.CharField(max_length=200)
-    image = models.FileField(upload_to=upload_to_image, null=True, blank=True)
+    image = models.ImageField(upload_to=upload_to_image, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
     pub_date = models.DateTimeField()
