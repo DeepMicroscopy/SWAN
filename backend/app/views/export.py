c967f269e7616ba16fa8f7ef462886aec8f13454
klassiker <6598117+klassiker@users.noreply.github.com>
2025-08-23 19:58:59 +0200

    Improve csv export closes #57

--- backend/app/views/export.py
+++ backend/app/views/export.py
@@ -1,57 +1,70 @@
 import csv
 
-from django.db.models import OuterRef, Subquery
+import itertools
+from django.db.models import OuterRef, Subquery, F
 from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema, OpenApiTypes, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
 
 from app import util
-from app.models import ClassificationUser, ClassificationAnonymous
+from app.models import ClassificationUser, ClassificationAnonymous, Study
 
+FIELD_NAMES = ["time", "study", "file", "choice", "label", "user_or_session"]
+TABLE_NAMES = ["date"] + FIELD_NAMES.copy()
+TABLE_NAMES.remove("time")
+TABLE_NAMES.remove("label")
 
-def get_data_users(study):
+def get_queryset_users(study):
     latest = ClassificationUser.objects.filter(
         user=OuterRef("user"),
         study=OuterRef("study"),
         index=OuterRef("index")
     ).order_by("-date").values("pk")[:1]
 
-    result = []
-
-    for entry in ClassificationUser.objects.filter(study_id=study, id__in=Subquery(latest)):
-        result.append(
-            [int(entry.date.timestamp()), entry.study.id, entry.file, entry.choice, entry.user.id]
-        )
-
-    return result
+    return ClassificationUser.objects.filter(study_id=study, id__in=Subquery(latest))
 
 
-def get_data_anonymous(study):
+def get_queryset_anonymous(study):
     latest = ClassificationAnonymous.objects.filter(
         session=OuterRef("session"),
         study=OuterRef("study"),
         index=OuterRef("index")
     ).order_by("-date").values("pk")[:1]
 
-    result = []
-
-    for entry in ClassificationAnonymous.objects.filter(study_id=study, id__in=Subquery(latest)):
-        result.append(
-            [int(entry.date.timestamp()), entry.study.id, entry.file, entry.choice, entry.session]
-        )
-
-    return result
+    return ClassificationAnonymous.objects.filter(study_id=study, id__in=Subquery(latest))
 
 
 def get_data(study):
-    users = get_data_users(study)
-    anonymous = get_data_anonymous(study)
+    users = get_queryset_users(study).annotate(
+        user_or_session=F("user"),
+    ).values(*TABLE_NAMES)
 
-    users.extend(anonymous)
+    anonymous = get_queryset_anonymous(study).annotate(
+        user_or_session=F("session"),
+    ).values(*TABLE_NAMES)
 
-    return users
+    queryset = Study.objects.all()
+    ui = get_object_or_404(queryset, pk=study).ui
+
+    def label(choice):
+        if choice in ui.labels:
+            if "label" in ui.labels[choice]:
+                return ui.labels[choice]["label"]
+            elif "text" in ui.labels[choice]:
+                return ui.labels[choice]["text"]
+
+        return None
+
+    def change(entry):
+        entry["time"] = entry["date"].timestamp()
+        entry["label"] = label(entry["choice"])
+        del entry["date"]
+        return entry
+
+    return list(map(change, itertools.chain(users, anonymous)))
 
 
 @extend_schema(tags=['Export'], parameters=[OpenApiParameter("study", OpenApiTypes.UUID, OpenApiParameter.PATH)])
@@ -66,10 +79,9 @@ class ExportViewSet(viewsets.ViewSet):
     @action(detail=True, renderer_classes=[util.SVGRenderer])
     def csv(self, request, study=None):
         response = HttpResponse(content_type="text/plain")
+        writer = csv.DictWriter(response, fieldnames=FIELD_NAMES)
 
-        writer = csv.writer(response)
-
-        writer.writerow(["time", "study", "file", "choice", "user_or_session"])
+        writer.writeheader()
         writer.writerows(get_data(study))
 
         return response
