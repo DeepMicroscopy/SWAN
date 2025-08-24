e1efee8b2537e4833fa5fdc0d8b6b74252205c4f
klassiker <6598117+klassiker@users.noreply.github.com>
2025-08-24 18:54:15 +0200

    Add cypress stubs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,1 +1,13 @@
-export { default } from 'eslint-config-vuetify/index.ts.mjs'
+import vuetifyConfig from 'eslint-config-vuetify/index.ts.mjs'
+import pluginCypress from 'eslint-plugin-cypress'
+
+export default [
+  ...vuetifyConfig,
+  pluginCypress.configs.recommended,
+  pluginCypress.configs.globals,
+  {
+    rules: {
+      'cypress/no-unnecessary-waiting': 'off',
+    },
+  },
+]
